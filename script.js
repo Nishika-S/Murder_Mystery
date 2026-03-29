162bdd995fc01694ac1d9fbf22b58d54fc599940
Nishika-S <seenundunsia@gmail.com>
2026-03-30 00:39:59 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function checkBinary() {
 
   if (input === "rafi") {
     document.getElementById("result").innerHTML =
-      "🔓 Folder unlocked. A voice memo transcript appears: <br><br>" +
+      "🔓 Folder unlocked.<br><br>" +
       "<em>'If you're reading this, I'm already gone. It was the coffee. I felt it immediately. I know who did it. I should have trusted my instincts about Rafi years ago. The book was the beginning. The poison was the end. The gold is where I first—'</em>" +
       "<br><br>" +
-      "The file cuts off. <br><br>" +
+      "<br><br>" +
       "🧩 Evidence Collected: Blackwell himself named Rafi. Poison confirmed. <br><br>" +
       "➡️ <a href='final.html' style='color:#00ffcc'>Return to FOA for the final verdict »</a>";
   } else {
